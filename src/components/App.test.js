f9a815e79f807244c62290447dbbd773e75b73bc
jihyunyang12 <jihyun.yang21@gmail.com>
2019-08-08 21:39:04 -0400

    First commit

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Contact from './LoginForm';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Contact />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
