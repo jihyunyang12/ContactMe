b8d6961fd222a7d400ad6e066082a5a278222cc0
jihyunyang12 <jihyun.yang21@gmail.com>
2019-08-21 00:42:27 -0400

    First commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,19 @@ import logo from '../logo.svg';
 import './App.css';
 import LoginForm from './LoginForm.js';
 import SignUpForm from './SignUpForm.js';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 function App() {
   return (
     <Router>
         <div className="App">
-            <h1>Welcome! abc</h1>
+            <h1>Welcome!</h1>
             <div>
-                <Route exact path="/LoginForm" component={LoginForm}></Route>
-                <Route exact path="/SignUpForm" component={SignUpForm}></Route>
+                <Link to ="/LoginForm"><button>Sign In</button></Link>
+                <Link to ="/SignUpForm"><button>Sign Up</button></Link>
             </div>
+            <Route exact path="/LoginForm" component={LoginForm}></Route>
+            <Route exact path="/SignUpForm" component={SignUpForm}></Route>
         </div>
     </Router>
   );
